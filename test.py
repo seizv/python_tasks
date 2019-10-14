92f7bb989c2b47df0958d296b68c4b16777e006d
seizv <vistaliiseiz.gmail.com>
2019-10-14 22:53:02 +0300

    stage3 start

--- test.py
+++ test.py
@@ -4,38 +4,6 @@ from ipmp.pages.rest.api import GuiApi
 import threading
 import json
 
-
-class SimpleEmu(object):
-    def __init__(self, ip='0.0.0.0', serial='1245ABCDEF90'):
-        self.ip = ip
-        self.serial = serial
-
-    def new_neo_panel(self, account='1234567890'):
-        self.account = account
-        '''
-        formatter = logging.Formatter('%(asctime)s::: %(message)s')
-        streamHandler = logging.StreamHandler()
-        streamHandler.setFormatter(formatter)
-        logger.addHandler(streamHandler)
-        '''
-        self.panel = NeoPanel(serial=self.serial, account=self.account, media='IP', model='HS3128', logger=logger)
-        self.panel.config.host = self.ip
-
-    def add_devices(self):
-        self.panel.add_device("CONTACT", 11) #device 1
-        #set alarm for device 1 zone 11
-        self.panel.set_device_alarm('tamper', 1, 11, 'detector')
-        self.panel.add_device("MOTION_SENSOR", 12) #device 2
-        # set trouble and alarm for device 2 zone 12
-        self.panel.set_device_trouble('low_battery', 1, 12, 'detector')
-        self.panel.set_device_alarm('tamper', 1, 12, 'detector')
-
-    def start_process(self):
-        self.panel.connectITv2()
-        self.panel.stopITv2Session()
-        self.panel.config.VK.active = False
-
-
 class RestAnswers(object):
     def __init__(self, ip='0.0.0.0', serial='1245ABCDEF90'):
         self.ip = ip
@@ -70,9 +38,17 @@ class RestAnswers(object):
             for i in response_list:
                 if i['warnings'] is not None:
                     for list_i in i['warnings']:
-                        if ['device_type'] == 'CONTROL_PANEL' and list_i['type'] == 'NOT_DISCOVERED':
+                        if i['device_type'] == 'CONTROL_PANEL' and list_i['type'] == 'NOT_DISCOVERED':
                             return False
             return True
+
+    def walk_test(self):
+        api = GuiApi(self.ip, logger=logger, formt='json')
+        api.Login.login(usr_email='admin@tycomonitor.com', usr_password='Admin123')
+        unt_id = api.Units.getUnitId(self.serial)
+        #response_list = api.Diagnostic.getDevices(unitId=unt_id)
+        response = api.Unit.info(unt_id=unt_id)
+        print(api.Units(unt_id, response))
 
 
 if __name__ == '__main__':
@@ -82,19 +58,8 @@ if __name__ == '__main__':
     ip = '94.125.123.68'
     serial = '1245ABCDEF96'
     #-----------create_classes-------------------
-    emu_test = SimpleEmu(ip=ip, serial=serial)
-    emu_test.new_neo_panel()
-    emu_test.add_devices()
     answers = RestAnswers(ip=ip, serial=serial)
     #------------start---------------------------
-    my_thread = threading.Thread(target=emu_test.start_process)
-    my_thread.start()
-    my_thread = threading.Thread(target=answers.show_warnings)
-    my_thread.start()
-
-
-
-
-
+    answers.walk_test()
 
 
