c01158df9357385839b11e6fa3eff12104bf6ac1
seizv <seizgames@gmail.com>
2019-10-31 16:02:05 +0200

    refactoring (fixed prc_id) ver1.01

--- script_for_bugs.py
+++ script_for_bugs.py
@@ -8,13 +8,13 @@ class DowloadConfig(object):
         self.ip = ip
         self.serial = serial
         self.account = account
-        self.api = GuiApi(self.ip, logger=logger, formt='json')
-        self.api.Login.login(usr_email='admin@tycomonitor.com', usr_password='Admin123')
+        self.api = api
 
 
     def create_panel(self):
         self.panel = NeoPanel(serial=self.serial, account=self.account, media='IP', model='HS3248', logger=logger)
         self.panel.config.host = self.ip
+        self.panel.config.system_account = serial[6:]
         #add devices with troubles
         self.panel.add_device("CONTACT", 11)  # device 1
         self.panel.set_device_alarm('tamper', 1, 11, 'detector')
@@ -38,10 +38,14 @@ class DowloadConfig(object):
             time.sleep(10)
             print('waiting for discovery finished {}'.format(self.serial))
         self.api.Configuration.refresh(unt_id=self.unt_id)
-        pr_id = self.api.Processes.getFirstAppearedWithNeededStatusProcessId('start')
+        data = self.api.Processes.getProcesses()
+        for process in data:
+            if process['unt_serial'] == self.serial and process['prs_status'] == 'start' and process['prs_type'] == 'ConfigDownload':
+                pr_id = process['prs_id']
         while self.api.Processes.getProcessStatus(pr_id) != 'succeeded':
             time.sleep(15)
             print('waiting for process succeeded {}'.format(self.serial))
+        print('Panel remove {}'.format(self.serial))
         self.api.Units.removeUnit(id=self.serial)
 
 
@@ -49,18 +53,17 @@ class DowloadConfig(object):
 if __name__ == '__main__':
     #logging.basicConfig(level='DEBUG')
     logger = logging.getLogger()
-    ip = '94.125.123.68'
+    ip = '94.125.123.69'
+    api = GuiApi(ip, logger=logger, formt='json')
+    api.Login.login(usr_email='admin@tycomonitor.com', usr_password='Admin123')
     n = 10
     serials = ['%X' % (i + 0xC00000080000) for i in range(n)]
-    objs = []
-    tasks = []
+    objs = tasks = []
     for i, serial in enumerate(serials):
-        objs.append(DowloadConfig(ip=ip, serial=serial))
-        my_thread = threading.Thread(target=objs[i].create_panel)
-        my_thread.start()
+        objs.append(DowloadConfig(ip=ip, serial=serial, account=serial[2:]))
+        objs[i].create_panel()
         my_thread = threading.Thread(target=objs[i].start_configuration_process)
         my_thread.start()
-        time.sleep(10)
-    print(objs)
+        #time.sleep(5)
     #response = api.Unit.info(unt_id = '%!@#$%^&*')
 
